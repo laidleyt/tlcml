c92197c5028f315d8b35a874affa0bd748f8d082
laidleyt <tom.laidley@gmail.com>
2025-06-12 16:33:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
+import os
 from dash import dcc, html, Input, Output, State, ctx
 import dash_bootstrap_components as dbc
 from dash.dependencies import ALL
@@ -244,4 +245,5 @@ def set_selected_subtab(n_clicks_list, id_list):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8050)
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port, debug=True)
