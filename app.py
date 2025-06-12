6cc566a9c0361db385d1893b6270455b53dab8f0
laidleyt <tom.laidley@gmail.com>
2025-06-12 16:40:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -246,4 +246,4 @@ def set_selected_subtab(n_clicks_list, id_list):
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8050))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
